3c2a58ad2a7b76d8422de044af5473b7de6c4e3f
miaoyu <miaoyu@sefonsoft.com>
2021-02-04 14:08:04 +0800

    style: add emotion

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,33 +1,13 @@
 import React from "react";
-import { cleanObject } from "utils";
 import { useAuth } from "context/auth-context";
-import { Button, Form, Input } from "antd";
-
-// interface Base {
-//   id: number
-// }
-//
-// interface Advance extends Base {
-//   name: string
-// }
-//
-// const test = (p: Base) => {
-// }
-//
-// // 鸭子类型(duck typing)：面向接口编程 而不是 面向对象编程
-// const a = {id: 1, name: 'jack'}
-// test(a)
+import { Form, Input } from "antd";
+import { LongButton } from "unauthenticated-app/index";
 
 export const LoginScreen = () => {
   const { login, user } = useAuth();
 
   // HTMLFormElement extends Element
   const handleSubmit = (values: { username: string; password: string }) => {
-    // event.preventDefault();
-    // const username = (event.currentTarget.elements[0] as HTMLInputElement)
-    //   .value;
-    // const password = (event.currentTarget.elements[1] as HTMLInputElement)
-    //   .value;
     login(values);
   };
 
@@ -46,9 +26,9 @@ export const LoginScreen = () => {
         <Input placeholder={"密码"} type="password" id={"password"} />
       </Form.Item>
       <Form.Item>
-        <Button htmlType={"submit"} type={"primary"}>
+        <LongButton htmlType={"submit"} type={"primary"}>
           登录
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,21 +1,8 @@
 import React from "react";
 import { useAuth } from "context/auth-context";
-import { Button, Form, Input } from "antd";
+import { Form, Input } from "antd";
+import { LongButton } from "unauthenticated-app/index";
 
-// interface Base {
-//   id: number
-// }
-//
-// interface Advance extends Base {
-//   name: string
-// }
-//
-// const test = (p: Base) => {
-// }
-//
-// // 鸭子类型(duck typing)：面向接口编程 而不是 面向对象编程
-// const a = {id: 1, name: 'jack'}
-// test(a)
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const RegisterScreen = () => {
@@ -41,9 +28,9 @@ export const RegisterScreen = () => {
         <Input placeholder={"密码"} type="password" id={"password"} />
       </Form.Item>
       <Form.Item>
-        <Button htmlType={"submit"} type={"primary"}>
+        <LongButton htmlType={"submit"} type={"primary"}>
           注册
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
