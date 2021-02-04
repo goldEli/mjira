fbfb905f55cc58dc6389bb76474983f87916b39c
miaoyu <miaoyu@sefonsoft.com>
2021-02-04 10:22:58 +0800

    feat: login、register、logout

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { AppProviders } from "context/index";
 import { loadDevTools } from "jira-dev-tool";
 
 loadDevTools(() => {
   ReactDOM.render(
     <React.StrictMode>
-      {/* <AppProviders> */}
-      <App />
-      {/* </AppProviders> */}
+      <AppProviders>
+        <App />
+      </AppProviders>
     </React.StrictMode>,
     document.getElementById("root")
   );
