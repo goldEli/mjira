71c839d4a56d78092cd1897944edaab45ad3253c
miaoyu <miaoyu@sefonsoft.com>
2021-02-05 09:18:06 +0800

    feat: handle me interface

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,4 +1,27 @@
 import styled from "@emotion/styled";
+import React from "react";
+import { Spin, Typography } from "antd";
+import { DevTools } from "jira-dev-tool";
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FullPageLoading = () => (
+  <FullPage>
+    <Spin size={"large"} />
+  </FullPage>
+);
+
+export const FullPageErrorFallback = ({ error }: { error: Error | null }) => (
+  <FullPage>
+    <DevTools />
+    <Typography.Text type={"danger"}>{error?.message}</Typography.Text>
+  </FullPage>
+);
 
 export const Row = styled.div<{
   gap?: number | boolean;
