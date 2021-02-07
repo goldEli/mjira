c85d3285b8d785eea91c987ebdc8c7717ef2136d
miaoyu <miaoyu@sefonsoft.com>
2021-02-07 14:59:57 +0800

    chore: remove test

--- src/screens/project-list/test.ts
+++
@@ -1,8 +0,0 @@
-import react from "React";
-
-export const useTest = (keys: string[]) => {
-  var url_string = window.location.href;
-  var url = new URL(url_string);
-  var c = url.searchParams.get("name");
-  console.log(c);
-};
